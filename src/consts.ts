5d9dbe75a9372ea6835bafc0c8a170e7d05c785d
PurplePulse <qshen98@gmail.com>
2026-01-20 23:07:15 +0800

    Change Site Title

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,4 @@
 // Site configuration
 
-export const SITE_TITLE = 'Marginalia';
+export const SITE_TITLE = 'Quotescript';
 export const SITE_DESCRIPTION = 'Notes on building, thinking, and the spaces in between.';
